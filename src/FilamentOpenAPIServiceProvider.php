13a44899d828ccd9decae112d85b0eb7eed7c8d0
WildEgo <WildEgo@users.noreply.github.com>
2024-03-02 22:44:48 +0000

    Fix styling

--- src/FilamentOpenAPIServiceProvider.php
+++ src/FilamentOpenAPIServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Evocative\FilamentOpenAPI;
 
+use Evocative\FilamentOpenAPI\Commands\FilamentOpenAPICommand;
+use Evocative\FilamentOpenAPI\Testing\TestsFilamentOpenAPI;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Evocative\FilamentOpenAPI\Commands\FilamentOpenAPICommand;
-use Evocative\FilamentOpenAPI\Testing\TestsFilamentOpenAPI;
 
 class FilamentOpenAPIServiceProvider extends PackageServiceProvider
 {
